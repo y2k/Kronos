fef3c3e8687149e933a1d2886219760e1b9daa99
y2k <itwisterlx@gmail.com>
2017-09-03 13:32:47 +0300

    ignore "not message" upates

--- Telegram.fs
+++ Telegram.fs
@@ -5,6 +5,11 @@ open Telegram.Bot.Types
 open TelegramAnswers
 open TelegramAnswers.Log
 
+let filterMessages (updates : Update seq) chatTitle = 
+    updates 
+    |> Seq.filter 
+           (fun x -> not (isNull x.Message) && x.Message.Chat.Title = chatTitle)
+
 let repl config action = 
     async { 
         let bot = TelegramBotClient(config.token)
@@ -16,16 +21,12 @@ let repl config action =
                 offset <- updates
                           |> Seq.map (fun x -> x.Id + 1)
                           |> Seq.fold (fun a x -> max a x) offset
-                let chatUpdates = 
-                    updates 
-                    |> Seq.filter (fun x -> x.Message.Chat.Title = config.chat)
+                let chatUpdates = filterMessages updates config.chat
                 for update in chatUpdates do
                     try 
-                        LOG
-                            (sprintf 
-                                 "Telegram update = %O, offset = %O, chat = %O" 
-                                 update.Message.Text offset 
-                                 update.Message.Chat.Title)
+                        sprintf "Telegram update = %O, offset = %O, chat = %O" 
+                            update.Message.Text offset update.Message.Chat.Title 
+                        |> LOG
                         do! action update
                     with e -> LOG(sprintf "Telegram error = %O" e)
             with e -> LOG(sprintf "Telegram error = %O" e)
